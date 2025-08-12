04974a7446a726dede5d45910b376875f0d2be5d
abhro <5664668+abhro@users.noreply.github.com>
2025-08-11 22:00:39 -0400

    Make data directory customizable in cr-momentum-slice-binormal-mom-fitter.jl

--- cr-momentum-slice-binormal-mom-fitter.jl
+++ cr-momentum-slice-binormal-mom-fitter.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.20.5
+# v0.20.14
 
 using Markdown
 using InteractiveUtils
@@ -93,11 +93,14 @@ md"""
 # Read data file
 """
 
+# ╔═╡ 22088c63-a7ac-45c0-97db-c1fc3360fe2d
+datadir = joinpath(ENV["DATADIR_ROOT"], "Processed-data");
+
 # ╔═╡ bdb9591b-b7ac-47e6-98bc-f18921bb64f9
-CR_p_gdf_momentum = load_object("Processed-data/dNdp-CR-protons-momentum-split.jld2")
+CR_p_gdf_momentum = load_object(joinpath(datadir, "dNdp-CR-protons-momentum-split.jld2"))
 
 # ╔═╡ 3777306e-eb41-413b-80a9-72cdc0228a94
-CR_e_gdf_momentum = load_object("Processed-data/dNdp-CR-electrons-momentum-split.jld2")
+CR_e_gdf_momentum = load_object(joinpath(datadir, "dNdp-CR-electrons-momentum-split.jld2"))
 
 # ╔═╡ 628130bf-da25-4799-8e5e-3d2db15b1e49
 md"""
@@ -626,6 +629,7 @@ BiNormalDistribution.componentpdfs(sciml_sol_dist, 3)
 # ╟─259697bd-dee5-432d-9377-a3d3c30b3564
 # ╟─4415022a-54dc-4f3d-a651-f66ae63dd051
 # ╟─8dfe6f3c-f693-4c73-8152-8c43c1c1ff42
+# ╠═22088c63-a7ac-45c0-97db-c1fc3360fe2d
 # ╠═bdb9591b-b7ac-47e6-98bc-f18921bb64f9
 # ╠═3777306e-eb41-413b-80a9-72cdc0228a94
 # ╟─628130bf-da25-4799-8e5e-3d2db15b1e49
