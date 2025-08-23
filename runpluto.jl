4abc80ac7aeffd32df6a435a16b7fa7bce26035d
abhro <5664668+abhro@users.noreply.github.com>
2025-08-23 17:17:58 -0400

    Make Pluto.jl watch for notebook changes outside the server

--- runpluto.jl
+++ runpluto.jl
@@ -7,6 +7,7 @@ cd("notebooks") do
         host = "0.0.0.0",
         port = 5999,
         launch_browser = "--browser" in Base.ARGS,
+        auto_reload_from_file = true,
         enable_ai_editor_features = false,
     )
 end
