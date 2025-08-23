43c105dd84fcc5bf6484639f934a28bde2341429
abhro <5664668+abhro@users.noreply.github.com>
2025-08-23 16:29:00 -0400

    Don't self-activate project environment in runpluto.jl
    Make it the responsibility of the user to select the project environment

--- runpluto.jl
+++ runpluto.jl
@@ -1,16 +1,12 @@
 #!julia
 
-import Pkg
-
-Pkg.activate(Base.current_project())
-
-cd("notebooks")
-
 import Pluto
 
-Pluto.run(;
-    host = "0.0.0.0",
-    port = 5999,
-    launch_browser = "--browser" in Base.ARGS,
-    enable_ai_editor_features = false,
-)
+cd("notebooks") do
+    Pluto.run(;
+        host = "0.0.0.0",
+        port = 5999,
+        launch_browser = "--browser" in Base.ARGS,
+        enable_ai_editor_features = false,
+    )
+end
