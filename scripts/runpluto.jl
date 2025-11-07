c4be8bdd14d4e58700f358537019ad003443bd35
abhro <5664668+abhro@users.noreply.github.com>
2025-11-07 10:50:37 -0500

    Add more CLI configuration options to scripts/runpluto.jl

--- scripts/runpluto.jl
+++ scripts/runpluto.jl
@@ -3,14 +3,33 @@
 using DrWatson
 @quickactivate "MCScatteringDataAnalysis"
 
+if "--help" in ARGS
+    println("Usage: $PROGRAM_FILE [--network] [--browser]")
+    exit()
+end
+
 import Pluto
 
 cd(projectdir("notebooks"))
 
+if "--network" in ARGS
+    # where to listen
+    host = "0.0.0.0"
+    # name of computer
+    hostname = Libc.gethostname()
+else
+    host = "127.0.0.1"
+    hostname = "localhost"
+end
+# port to listen to, taken from thesis course number
+port = 5999
+launch_browser = "--browser" in ARGS
+
 Pluto.run(;
-    host = "0.0.0.0",
-    port = 5999,
-    launch_browser = "--browser" in Base.ARGS,
+    host,
+    port,
+    root_url = "http://$hostname:$port/",
+    launch_browser,
     auto_reload_from_file = true,
     enable_ai_editor_features = false,
 )
