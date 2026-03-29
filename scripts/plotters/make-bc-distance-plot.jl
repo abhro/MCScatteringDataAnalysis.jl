06b37d7c855897ea7bd1bb4b69c623248fb6c216
abhro <5664668+abhro@users.noreply.github.com>
2026-03-28 20:55:11 -0400

    Make Bhattacharya distance plots for multiple bins at once (#5)

--- scripts/plotters/make-bc-distance-plot.jl
+++ scripts/plotters/make-bc-distance-plot.jl
@@ -27,11 +27,12 @@ function (@main)(args)
     bins = args["bins"]
 
     yscale = args["log-scale"] ? log10 : identity
+    should_title = args["title"]
 
     CR_p_gdf_momentum_filename = joinpath(dir, "dNdp-CR-protons-momentum-split.jld2")
 
     @info("Starting plots of protons")
-    proton_plots(CR_p_gdf_momentum_filename; bins, outdir)
+    species_plots(CR_p_gdf_momentum_filename; species_name = "protons", bins, outdir, should_title)
     @info("Finished plots of protons")
 
     GC.gc()
@@ -39,50 +40,63 @@ function (@main)(args)
     CR_e_gdf_momentum_filename = joinpath(dir, "dNdp-CR-electrons-momentum-split.jld2")
 
     @info("Starting plots of electrons")
-    electron_plots(CR_e_gdf_momentum_filename; bins, outdir)
+    species_plots(CR_e_gdf_momentum_filename; species_name = "electrons", bins, outdir, should_title)
     @info("Finished plots of electrons")
 
     return
 end
 
-function proton_plots(CR_p_gdf_momentum_filename; bins, outdir)
-    CR_p_gdf_momentum = load_object(CR_p_gdf_momentum_filename)
-    proton_log_p_nat = keys(CR_p_gdf_momentum) .|> values .|> first
-    # create histogram for each of the momentum slices
-    normal_distrib_protons_from_curves = fitdistributions(v -> fit_dist_to_histogram(Normal, v; nbins = bins), CR_p_gdf_momentum)
-    # create MLE fitted distribution for each of the slices
-    normal_distrib_protons = fitdistributions(v -> fitdistribution(Normal, v), CR_p_gdf_momentum)
-    proton_distances = bcdistances(normal_distrib_protons.pf, normal_distrib_protons_from_curves.pf)
+function species_plots(CR_gdf_filename; species_name, bins, outdir, should_title)
+    (log_p_nat, mle_fit_dists, hist_fit_dists) = dist_fits(CR_gdf_filename, bins)
+    @info("Fitted $species_name data to distributions")
+    @debug("Received fits", mle_fit_dists, hist_fit_dists)
     fig = Figure()
     ax = Axis(fig[1, 1]; axis_properties...)
-    scatterlines!(ax, proton_log_p_nat, proton_distances; label = "protons, plasma frame", markersize, color = color_pf_p)
-    hlines!(ax, 2.0e-3, linewidth = 0.4, color = color_pf_p, linestyle = :dash)
+    if should_title
+        ax.title = plot_title
+    end
+    hlines!(ax, 2.0e-3, linewidth = 0.8, linestyle = :dash)
+    for (bin_count, dist) in zip(bins, hist_fit_dists)
+        @debug("Processing bin count=$bin_count", dist)
+        distances = bcdistances(mle_fit_dists.pf, dist.pf)
+        scatterlines!(ax, log_p_nat, distances; label = "bins = $bin_count", markersize)
+    end
     axislegend(ax, position = :ct, framevisible = false)
-    save(joinpath(outdir, "bc-distances-protons-$bins-bins.svg"), fig)
+    plot_filename = "bc-distances-$species_name-$(join(bins, ","))-bins.svg"
+    save(joinpath(outdir, plot_filename), fig)
     return
 end
 
-function electron_plots(CR_e_gdf_momentum_filename; bins, outdir)
-    CR_e_gdf_momentum = load_object(CR_e_gdf_momentum_filename)
-    electron_log_p_nat = keys(CR_e_gdf_momentum) .|> values .|> first
-    normal_distrib_electrons = fitdistributions(v -> fitdistribution(Normal, v), CR_e_gdf_momentum)
-    normal_distrib_electrons_from_curves = fitdistributions(v -> fit_dist_to_histogram(Normal, v; nbins = bins), CR_e_gdf_momentum)
-    electron_distances = bcdistances(normal_distrib_electrons.pf, normal_distrib_electrons_from_curves.pf)
-    fig = Figure()
-    ax = Axis(fig[1, 1]; axis_properties...)
-    scatterlines!(ax, electron_log_p_nat, electron_distances; label = "electrons, plasma frame", markersize, color = color_pf_e)
-    hlines!(ax, 2.0e-3, linewidth = 0.4, color = color_pf_e, linestyle = :dash)
-    axislegend(ax, position = :ct, framevisible = false)
-    save(joinpath(outdir, "bc-distances-electrons-$bins-bins.svg"), fig)
-    return
+"""
+Read a file, for each momentum slice, make an MLE fit and a bunch of histogram
+fits based on number of bins provided
+"""
+function dist_fits(filename, bins)
+    df = load_object(filename)
+    log_p = keys(df) .|> values .|> first
+    # MLE fitted distribution
+    mle = fitdistributions(v -> fitdistribution(Normal, v), df)
+    # distributions fitted through least squares on histogram curve
+    hist_dists = []
+    for bin_count in bins
+        push!(
+            hist_dists,
+            fitdistributions(
+                v -> fit_dist_to_histogram(Normal, v; nbins = bin_count),
+                df,
+            )
+        )
+    end
+    return (; log_p, mle, hist_dists)
 end
 
+
+const plot_title = "Distribution agreement curve"
 const axis_properties = (;
     xminorgridvisible = true,
     yminorgridvisible = true,
     xminorticksvisible = true,
     yminorticksvisible = true,
-    title = "Distribution agreement curve",
     xlabel = "log p (nat)",
     ylabel = "Bhattacharya distance",
     yscale = log10,
@@ -94,7 +108,9 @@ const color_pf_p, color_sf_p, color_ISM_p, color_pf_e, color_sf_e, color_ISM_e =
 Create and return an ArgParse.jl arg table
 """
 function get_parser()
-    s = ArgParseSettings()
+    s = ArgParseSettings(
+        help_width = 120
+    )
 
     add_arg_table!(
         s,
@@ -106,8 +122,9 @@ function get_parser()
         "--bins",
         Dict(
             :help => "Number of bins to use for the histogram",
-            :default => 90,
+            :default => [90],
             :arg_type => Int64,
+            :nargs => '+',
             :metavar => "N",
         ),
         "--column",
@@ -115,6 +132,9 @@ function get_parser()
 
         "--log-scale",
         Dict(:help => "Whether the y-axis should be in log-scale", :action => :store_true),
+
+        "--title",
+        Dict(:help => "Whether we should add a title to the figure", :action => :store_true),
     )
     return s
 end
