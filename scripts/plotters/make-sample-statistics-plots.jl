9693919b2aeb9cb5b62aa28a658099d66c5533e6
abhro <5664668+abhro@users.noreply.github.com>
2026-04-16 13:32:52 -0400

    Add vertical cutoffs to error bands (#6)

--- scripts/plotters/make-sample-statistics-plots.jl
+++ scripts/plotters/make-sample-statistics-plots.jl
@@ -60,19 +60,37 @@ function (@main)(args = [])
     p_std_devs = proton_stats.std_devs
     e_std_devs = electron_stats.std_devs
 
+    proton_cutoffs = tryparse.(Float64, args["proton-cutoffs"])
+    electron_cutoffs = tryparse.(Float64, args["electron-cutoffs"])
+
+    # List of cutoffs for shock γ:
+    # γ = 5: Protons: 7.2; Electrons: -0.4, 6.95
+    # γ = 2, low XNPER: Protons: 0.05, 5.75; Electrons: 1.55, 6.15
+    # γ = 2, high XNPER: Protons: 2.05, 5.7; Electrons: 0.9, 6.05
+    # γ = 10: Protons: 0.95, 7.35; Electrons: 0.65, 7.25
+    # γ = 20: Protons: 1.25, 8.25; Electrons: 0.95, 6.95
+
     # mean plot
     @info("Creating mean plots ($(now()))")
     make_sample_stat_plots(
-        log_pₚ, log_pₑ, p_means, e_means;
+        log_pₚ, log_pₑ, p_means, e_means, proton_cutoffs, electron_cutoffs;
         outdir, should_title, stat_title = "mean", ylabel = L"$⟨\log\,n_p⟩$"
     )
 
     @info("Creating std. dev. plots ($(now()))")
-    make_std_dev_plots(log_pₚ, log_pₑ, p_std_devs, e_std_devs; outdir, should_title)
+    make_std_dev_plots(
+        log_pₚ, log_pₑ, p_std_devs, e_std_devs,
+        proton_cutoffs, electron_cutoffs;
+        outdir, should_title,
+    )
 
     # mean with std_dev envelope
     @info("Creating mean with std. dev. envelope plots ($(now()))")
-    make_envelope_plots(log_pₚ, log_pₑ, p_means, e_means, p_std_devs, e_std_devs; outdir, should_title)
+    make_envelope_plots(
+        log_pₚ, log_pₑ, p_means, e_means, p_std_devs, e_std_devs,
+        proton_cutoffs, electron_cutoffs;
+        outdir, should_title,
+    )
 
     # skewness
     @info("Creating skewness plots ($(now()))")
@@ -125,20 +143,29 @@ Create proton, electron, and combined plots for a particular sample statistic
 - `log_pₑ`: Vector containing (log of) electron momenta in mp*c units
 - `p_stat`: Sample statistic corresponding to each proton momentum
 - `e_stat`: Sample statistic corresponding to each slice momentum
+- `proton_cutoffs`: Vertical line(s) to plot with proton colors
+- `electron_cutoffs`: Vertical line(s) to plot with electron colors
 - `outdir`: Directory where the plots should be saved
 - `stat_title`: Name of the sample statistic (mean/kurtosis/etc...)
 - `ylabel`: Label for the plots' y-axes
 """
-function make_sample_stat_plots(log_pₚ, log_pₑ, p_stat, e_stat; outdir, stat_title, ylabel, should_title)
+function make_sample_stat_plots(
+        log_pₚ, log_pₑ, p_stat, e_stat, proton_cutoffs, electron_cutoffs;
+        outdir, stat_title, ylabel, should_title
+    )
     species_map = [
-        ("protons", log_pₚ, p_stat, color_pf_p),
-        ("electrons", log_pₑ, e_stat, color_pf_e),
+        ("protons", log_pₚ, p_stat, proton_cutoffs, color_pf_p),
+        ("electrons", log_pₑ, e_stat, electron_cutoffs, color_pf_e),
     ]
-    for (species, log_p, stat, color) in species_map
+
+    for (species, log_p, stat, cutoffs, color) in species_map
         @info("Making $species plot")
         fig, ax = make_figax(; stat_title, ylabel, should_title)
         lines!(ax, log_p, stat; color, label = species)
         ax.xticks = unit_ticks(log_p)
+        if !isnothing(cutoffs) && length(cutoffs) > 0
+            vlines!(ax, cutoffs; color)
+        end
         #axislegend(ax) #, framevisible = false
         save(joinpath(outdir, "$species-$stat_title.svg"), fig)
     end
@@ -149,26 +176,40 @@ function make_sample_stat_plots(log_pₚ, log_pₑ, p_stat, e_stat; outdir, stat
     ax.xticks = unit_ticks(vcat(log_pₚ, log_pₑ))
     lines!(ax, log_pₚ, p_stat, color = color_pf_p, label = "protons")
     lines!(ax, log_pₑ, e_stat, color = color_pf_e, label = "electrons")
+
+    if !isnothing(proton_cutoffs) && length(proton_cutoffs) > 0
+        vlines!(ax, proton_cutoffs, color = color_pf_p)
+    end
+    if !isnothing(electron_cutoffs) && length(electron_cutoffs) > 0
+        vlines!(ax, electron_cutoffs, color = color_pf_e, linestyle = :dash)
+    end
+
     axislegend(ax) #, framevisible = false
     save(joinpath(outdir, "combined-$stat_title.svg"), fig)
     return
 end
 
-function make_std_dev_plots(log_pₚ, log_pₑ, σₚ, σₑ; outdir, should_title)
+function make_std_dev_plots(
+        log_pₚ, log_pₑ, σₚ, σₑ, proton_cutoffs, electron_cutoffs;
+        outdir, should_title
+    )
     # plotting configs
     stat_title = "standard deviation"
     ylabel = L"σ"
     species_map = [
-        ("protons", log_pₚ, σₚ, color_pf_p),
-        ("electrons", log_pₑ, σₑ, color_pf_e),
+        ("protons", log_pₚ, σₚ, proton_cutoffs, color_pf_p),
+        ("electrons", log_pₑ, σₑ, electron_cutoffs, color_pf_e),
     ]
 
-    for (species, log_p, σ, color) in species_map
+    for (species, log_p, σ, cutoffs, color) in species_map
         @info("Making $species plot")
         fig, ax = make_figax(; stat_title, ylabel, should_title)
         ax.xticks = unit_ticks(log_p)
         lines!(ax, log_p, σ; color, label = species)
         #axislegend(ax, framevisible = false, position = :lt)
+        if !isnothing(cutoffs) && length(cutoffs) > 0
+            vlines!(ax, cutoffs; color)
+        end
         save(joinpath(outdir, "$species-std-devs.svg"), fig)
         ax.yscale = log10
         save(joinpath(outdir, "$species-std-devs-logscale.svg"), fig)
@@ -181,13 +222,24 @@ function make_std_dev_plots(log_pₚ, log_pₑ, σₚ, σₑ; outdir, should_tit
     lines!(ax, log_pₚ, σₚ, color = color_pf_p, label = "protons")
     lines!(ax, log_pₑ, σₑ, color = color_pf_e, label = "electrons")
     axislegend(ax, position = :lt) #, framevisible = false
+
+    if !isnothing(proton_cutoffs) && length(proton_cutoffs) > 0
+        vlines!(ax, proton_cutoffs, color = color_pf_p)
+    end
+    if !isnothing(electron_cutoffs) && length(electron_cutoffs) > 0
+        vlines!(ax, electron_cutoffs, color = color_pf_e, linestyle = :dash)
+    end
+
     save(joinpath(outdir, "combined-std-devs.svg"), fig)
     ax.yscale = log10
     save(joinpath(outdir, "combined-std-devs-logscale.svg"), fig)
     return
 end
 
-function make_envelope_plots(log_pₚ, log_pₑ, μₚ, μₑ, σₚ, σₑ; outdir, should_title)
+function make_envelope_plots(
+        log_pₚ, log_pₑ, μₚ, μₑ, σₚ, σₑ, proton_cutoffs, electron_cutoffs;
+        outdir, should_title,
+    )
     # plotting configs
     stat_title = "mean"
     ylabel = L"$⟨\log\,n_p⟩$"
@@ -214,7 +266,15 @@ function make_envelope_plots(log_pₚ, log_pₑ, μₚ, μₑ, σₚ, σₑ; out
     band!(ax, log_pₚ, μₚ + σₚ, μₚ - σₚ; alpha, color = color_pf_p, label = "protons")
     lines!(ax, log_pₑ, μₑ, color = color_pf_e, label = "electrons")
     band!(ax, log_pₑ, μₑ + σₑ, μₑ - σₑ; alpha, color = color_pf_e, label = "electrons")
-    axislegend(ax, merge = true) #, framevisible = false
+    axislegend(ax, merge = true, position = :cb) #, framevisible = false
+
+    if !isnothing(proton_cutoffs) && length(proton_cutoffs) > 0
+        vlines!(ax, proton_cutoffs, color = color_pf_p)
+    end
+    if !isnothing(electron_cutoffs) && length(electron_cutoffs) > 0
+        vlines!(ax, electron_cutoffs, color = color_pf_e, linestyle = :dash)
+    end
+
     save(joinpath(outdir, "combined-means-w-envelope.svg"), fig)
     return
 end
@@ -243,6 +303,12 @@ function get_parser()
 
         "--column",
         Dict(:help => "Which column of the dataframe to take statistics off", :default => :log_dNdp_cr_pf, :metavar => "COL"),
+
+        "--proton-cutoffs",
+        Dict(:help => "Location where to place the cutoff of protons", :nargs => '+', :metavar => "logp"),
+
+        "--electron-cutoffs",
+        Dict(:help => "Location where to place the cutoff of electrons", :nargs => '+', :metavar => "logp"),
 
         "--title",
         Dict(:help => "Whether we should add a title to each figure", :action => :store_true),
